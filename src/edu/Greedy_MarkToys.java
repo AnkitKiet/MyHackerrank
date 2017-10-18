61eccb573760d66864d4a41f6a45f76e0ac0f093
Ankit Maurya <ankit.1410025@kiet.edu>
2017-10-19 01:36:23 +0530

    #greedySolution

---
+++ src/edu/Greedy_MarkToys.java
@@ -0,0 +1,26 @@
+package edu;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greedy_MarkToys {
+
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k=scanner.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int sum=0;int count=0;
+        for(int i=0;i<n && sum+arr[i]<=k;i++){
+            sum+=arr[i];
+            count++:
+        }
+
+    }
+
+}
